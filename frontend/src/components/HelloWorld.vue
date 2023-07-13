5cb942ec14046b005273b4dc64a0c57ccdf5d150
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-07-13 10:50:42 +0900

    header, main 영역 추가

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Where To Play</h1>
-  <h2>주변 운동장 정보를 찾고 싶을 때</h2>
+  <header>
+    <h1 id="title">Where To Play</h1>
+    <h2 id="title">주변 운동장 정보를 찾고 싶을 때</h2>
+  </header>
+  <main>
+    <p class="about">ABOUT US</p>
+    <p></p>
+  </main>
 </template>
 
 <script>
