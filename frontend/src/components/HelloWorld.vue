637dd3f6cf04b4f1ce6245796f9fe6736c882a97
spooder <yoshi7591@naver.com>
2023-07-14 15:29:09 +0900

    contribute

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -3,10 +3,7 @@
     <h1>Where To Play</h1>
     <h2>"주변 운동장 정보를 찾고 싶을 때"</h2>
   </header>
-  <main>
     <div class="about"><b>ABOUT US</b></div>
-    <div class="aboutus"></div>
-  </main>
 </template>
 
 <script>
