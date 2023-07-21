aa9bf4d671a51e8f391bbd581510bed2287419a2
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-07-22 01:45:55 +0900

    update frontend / UI 개선

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,42 +1,38 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="ko">
-  <head>
-    <meta charset="UTF-8">
-    <title>Where To Play</title>
-  </head>
-  <body>
-    <div id="container">    
-      <header>
-        <div id="logo">
-          <a href="#"><h1>Where To Play</h1></a>
-        </div>
-        <nav>
-          <ul id="topMenu">
-            <li><a href="#">회원가입</a></li>
-            <li><a href="#">로그인</a></li>
-          </ul>
-        </nav>
-      </header>
-      <body>
-        <p id="description">Where To Play?</p>
-        <h1>#주변 운동장을 쉽게 찾고 싶을 때</h1>
-        <h1>#통합된 서비스</h1>
-        <h1>#예약 링크까지 바로 이동</h1>
-        <button id="start">시작 하기</button>
-      </body>
-      <footer>    
-        <section id="bottomMenu">
-          <ul>
-            <li><a href="#">About US</a></li>
-            <li><a href="#">개인정보처리방침</a></li>
-            <li><a href="#">사이트맵</a></li>
-          </ul>
-        </section>   
-      </footer>  
-    </div> 
-  </body>
-  </html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>Where To Play</title>
+</head>
+<body>
+  <div id="container">    
+    <header>
+      <div id="logo">
+        <a href="#"><h1>Where To Play</h1></a>
+      </div>
+      <nav>
+        <ul id="topMenu">
+          <li><a href="#">메인</a></li>
+          <li><a href="#">운동장 찾기</a></li>
+          <li><a href="#">운동장 등록</a></li>
+          <li><a href="#">테마 지도</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <footer>    
+      <section id="bottomMenu">
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">개인정보처리방침</a></li>
+          <li><a href="#">이용약관</a></li>
+          <li><a href="#">사이트맵</a></li>
+        </ul>
+      </section>   
+    </footer>  
+  </div> 
+</body>
+</html>
 </template>
 
 <script>
@@ -45,6 +41,7 @@
 
 
 <style>
+
 * {
   margin:0;   /* 마진 리셋 */
   padding:0;   /* 패딩 리셋 */
@@ -70,8 +67,6 @@ header{
   width:100%;    /*  너비 */
   height:100px;   /* 높이 */ 
   background-color:rgb(71, 110, 169);
-  
-  
 }
 
 /* 로고 */
@@ -89,27 +84,18 @@ header{
   text-shadow:0 -1px 0 #222;  /* 그림자 색 */
 }
 
-#description {
-  font-weight: 700;
-  font-size: 70px;
-}
-#start {
-  font-weight: 700;
-  font-size: 50px;
-}
 /* 내비게이션 */
 nav{  
-  float:right;   /* 오른쪽으로 플로팅 */
+  float:left;   /* 오른쪽으로 플로팅 */
   width:900px;   /*   너비 */  
-  height:100px;   /* 메뉴 영역 높이 */  
-  padding-top:40px;  /* 메뉴를 아래로 내리기 위해 */  
+  height: 100px;
+  line-height: 70px;
 }
 #topMenu {  
   height:60px; 
 }
 #topMenu > li {
-  float:right;  /* 메뉴 항목을 왼쪽으로 플로팅 */ 
-  
+  float:left;  /* 메뉴 항목을 왼쪽으로 플로팅 */ 
   position: relative;  
 }
 #topMenu > li > a{
@@ -118,14 +104,11 @@ nav{
   color:#fff;  /* 글자 색 */
   font-weight:600;  /* 글자 굵기 */
   padding:20px 60px;  /* 패딩 */ 
-
 }
 #topMenu > li > a:hover {
   color:#1fa8f8;
   text-shadow:0 -2px #222;
 }
-
-
 
 
 footer{   
