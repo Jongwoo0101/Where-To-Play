f2bbb1d84f8a3f567af5d948286663d33724bcf2
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-07-21 19:32:34 +0900

    헤더 영역 구성 / 헤더 영역 채우기 기능 필요

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,9 +1,35 @@
 <template>
-  <header>
-    <h1>Where To Play</h1>
-    <h2>"주변 운동장 정보를 찾고 싶을 때"</h2>
-  </header>
-    <div class="about"><b>ABOUT US</b></div>
+  <!DOCTYPE html>
+  <html lang="ko">
+  <head>
+    <meta charset="UTF-8">
+    <title>Where To Play</title>
+  </head>
+  <body>
+    <div id="container">    
+      <header>
+        <div id="logo">
+          <a href="#"><h1>Where To Play</h1></a>
+        </div>
+        <nav>
+          <ul id="topMenu">
+            <li><a href="#">회원가입</a></li>
+            <li><a href="#">로그인</a></li>
+          </ul>
+        </nav>
+      </header>
+      <footer>    
+        <section id="bottomMenu">
+          <ul>
+            <li><a href="#">About US</a></li>
+            <li><a href="#">개인정보처리방침</a></li>
+            <li><a href="#">사이트맵</a></li>
+          </ul>
+        </section>   
+      </footer>  
+    </div> 
+  </body>
+  </html>
 </template>
 
 <script>
@@ -12,17 +38,102 @@
 
 
 <style>
-h1 {
-  font-size: 50pt;
+* {
+  margin:0;   /* 마진 리셋 */
+  padding:0;   /* 패딩 리셋 */
+  box-sizing: border-box;   /* 박스 영역은 테두리까지 */ 
 }
-h2 {
-  padding: 5px;
+a {
+  text-decoration:none;  /* 텍스트 링크 밑줄 없애기 */ 
 }
-.about {
-  font-size: large;
+ul {
+  list-style: none;
 }
 
-.aboutus {
-  font-size: medium;
+#container {
+  margin:0 auto;  /* 화면 중앙에 배치 */
+  width:1200px;    /* 너비 */
 }
+
+/*********************************/
+/* 헤더 영역 - 로고와 내비게이션 */
+/*********************************/
+
+header{
+  width:100%;    /*  너비 */
+  height:100px;   /* 높이 */ 
+  background-color:rgb(71, 110, 169);
+  
+}
+
+/* 로고 */
+#logo{ 
+  float:left;   /* 왼쪽으로 플로팅 */ 
+  width:250px;   /*  너비 */ 
+  height:100px;   /* 높이 */
+  line-height:100px;  /* 세로로 중간에 맞춤 - 줄간격을 높이 값과 같게 */
+  padding-left:20px;  /* 왼쪽에 여백 */
+}
+#logo h1 {
+  font-family:'Merienda', cursive;
+  font-weight:700;
+  font-size:30px;   /* 글자 크기 */
+  color:#fff;   /* 글자 색*/
+  text-shadow:0 -1px 0 #222;  /* 그림자 색 */
+}
+
+/* 내비게이션 */
+nav{  
+  float:right;   /* 오른쪽으로 플로팅 */
+  width:900px;   /*   너비 */  
+  height:100px;   /* 메뉴 영역 높이 */  
+  padding-top:40px;  /* 메뉴를 아래로 내리기 위해 */  
+}
+#topMenu {  
+  height:60px; 
+}
+#topMenu > li {
+  float:right;  /* 메뉴 항목을 왼쪽으로 플로팅 */ 
+  position: relative;  
+}
+#topMenu > li > a{
+  display:block;  /* 링크 텍스트는 블록으로 */ 
+  font-size:1.1em;
+  color:#fff;  /* 글자 색 */
+  font-weight:600;  /* 글자 굵기 */
+  padding:20px 60px;  /* 패딩 */ 
+}
+#topMenu > li > a:hover {
+  color:#1fa8f8;
+  text-shadow:0 -2px #222;
+}
+
+
+
+
+footer{   
+  width:1200px;   /* 너비 */
+  height:100px;  /* 높이 */
+  border-top:2px solid #222;
+}
+#bottomMenu{
+  width:100%;
+  height:20px;
+  margin-left:60px;
+}
+#bottomMenu ul {
+  margin-top:15px;  
+}
+#bottomMenu ul li{
+  float:left;  /* 가로로 배치 */ 
+  padding:5px 20px;
+  border-right:1px solid #ddd;   /* 항목 오른쪽에 테두리 */ 
+}
+#bottomMenu ul li:last-child{
+  border:none;  /* 마지막 항목에는 오른쪽 테두리 없음 */  
+}
+#bottomMenu ul li a, #bottomMenu ul li a:visited {  
+  font-size:15px;  /* 글자 크기 */ 
+  color:#666;   /* 글자 색 */ 
+} 
 </style>
