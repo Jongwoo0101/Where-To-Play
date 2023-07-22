e9c8492e08e2222b2da811fe8ef94e894eb2252e
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-07-23 02:34:56 +0900

    코드 정리

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,14 +1,8 @@
 <template>
-<html lang="ko">
-<head>
-  <meta charset="UTF-8">
-  <title>Where To Play</title>
-</head>
-<body>
-  <div id="container">    
+  <div>
     <header>
       <div id="logo">
-        <a href="#"><h1>Where To Play</h1></a>
+        <h1>Where To Play</h1>
       </div>
       <nav>
         <ul id="topMenu">
@@ -19,8 +13,8 @@
         </ul>
       </nav>
     </header>
-    
-    <footer>    
+
+    <footer>
       <section id="bottomMenu">
         <ul>
           <li><a href="#">About Us</a></li>
@@ -28,112 +22,114 @@
           <li><a href="#">이용약관</a></li>
           <li><a href="#">사이트맵</a></li>
         </ul>
-      </section>   
-    </footer>  
-  </div> 
-</body>
-</html>
+      </section>
+    </footer>
+  </div>
 </template>
 
 <script>
 
 </script>
 
-
 <style>
-
 * {
-  margin:0;   /* 마진 리셋 */
-  padding:0;   /* 패딩 리셋 */
-  box-sizing: border-box;   /* 박스 영역은 테두리까지 */ 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
 a {
-  text-decoration:none;  /* 텍스트 링크 밑줄 없애기 */ 
+  text-decoration: none;
 }
+
 ul {
   list-style: none;
 }
 
 #container {
-  margin:0 auto;  /* 화면 중앙에 배치 */
-  width:100%;    /* 너비 */
+  margin: 0 auto;
+  width: 100%;
 }
 
-/*********************************/
-/* 헤더 영역 - 로고와 내비게이션 */
-/*********************************/
-
-header{
-  width:100%;    /*  너비 */
-  height:100px;   /* 높이 */ 
-  background-color:rgb(71, 110, 169);
+header {
+  width: 100%;
+  height: 100px;
+  background-color: rgb(71, 110, 169);
 }
 
-/* 로고 */
-#logo{ 
-  float:left;   /* 왼쪽으로 플로팅 */ 
-  width:250px;   /*  너비 */ 
-  height:100px;   /* 높이 */
-  line-height:100px;  /* 세로로 중간에 맞춤 - 줄간격을 높이 값과 같게 */
-  padding-left:20px;  /* 왼쪽에 여백 */
+#logo {
+  float: left;
+  width: 250px;
+  height: 100px;
+  line-height: 100px;
+  padding-left: 20px;
 }
+
 #logo h1 {
-  font-weight:700;
-  font-size:30px;   /* 글자 크기 */
-  color:#fff;   /* 글자 색*/
-  text-shadow:0 -1px 0 #222;  /* 그림자 색 */
+  font-weight: 700;
+  font-size: 30px;
+  color: #fff;
+  text-shadow: 0 -1px 0 #222;
 }
 
-/* 내비게이션 */
-nav{  
-  float:left;   /* 오른쪽으로 플로팅 */
-  width:900px;   /*   너비 */  
+nav {
+  float: left;
+  width: 900px;
   height: 100px;
   line-height: 70px;
 }
-#topMenu {  
-  height:60px; 
+
+#topMenu {
+  height: 60px;
 }
+
 #topMenu > li {
-  float:left;  /* 메뉴 항목을 왼쪽으로 플로팅 */ 
-  position: relative;  
+  float: left;
+  position: relative;
 }
-#topMenu > li > a{
-  display:block;  /* 링크 텍스트는 블록으로 */ 
-  font-size:1.1em;
-  color:#fff;  /* 글자 색 */
-  font-weight:600;  /* 글자 굵기 */
-  padding:20px 60px;  /* 패딩 */ 
+
+#topMenu > li > a {
+  display: block;
+  font-size: 1.1em;
+  color: #fff;
+  font-weight: 600;
+  padding: 20px 60px;
 }
+
 #topMenu > li > a:hover {
-  color:#1fa8f8;
-  text-shadow:0 -2px #222;
+  color: #1fa8f8;
+  text-shadow: 0 -2px #222;
 }
 
+footer {
+  width: 100%;
+  height: 100px;
+  border-top: 2px solid #222;
+}
 
-footer{   
-  width:100%;   /* 너비 */
-  height:100px;  /* 높이 */
-  border-top:2px solid #222;
+#bottomMenu {
+  width: 100%;
+  height: 20px;
+  margin-left: 60px;
 }
-#bottomMenu{
-  width:100%;
-  height:20px;
-  margin-left:60px;
-}
+
 #bottomMenu ul {
-  margin-top:15px;  
+  margin-top: 15px;
 }
-#bottomMenu ul li{
-  float:left;  /* 가로로 배치 */ 
-  padding:5px 20px;
-  border-right:1px solid #ddd;   /* 항목 오른쪽에 테두리 */ 
+
+#bottomMenu ul li {
+  float: left;
+  padding: 5px 20px;
+  border-right: 1px solid #ddd;
 }
-#bottomMenu ul li:last-child{
-  border:none;  /* 마지막 항목에는 오른쪽 테두리 없음 */  
+
+#bottomMenu ul li:last-child {
+  border: none;
 }
-#bottomMenu ul li a, #bottomMenu ul li a:visited {  
-  font-size:15px;  /* 글자 크기 */ 
-  color:#666;   /* 글자 색 */ 
-} 
+
+#bottomMenu ul li a,
+#bottomMenu ul li a:visited {
+  font-size: 15px;
+  color: #666;
+}
 </style>
