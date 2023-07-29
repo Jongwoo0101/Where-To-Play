99dee8ddfdbd753d850fe3f10ea9ba15cf60be0e
Spooder02 <yoshi7591@naver.com>
2023-07-30 00:44:21 +0900

    메인 페이지 임의 구성, Bulma CSS Framework 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
-
+    <div class="content is-large">
+      <h2>반갑습니다, WhereToPlay입니다.</h2>
+      <p>당신에게 알맞는 운동장을 실시간으로 확인해보세요!</p>
+      <button class="button is-link">지금 시작하기</button>
+    </div>
   </div>
 </template>
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+  .home {
+    
+  }
+</style>
 
 <script>
 export default {
   name: 'HomeView',
   components: {
-
+    
   }
 }
 </script>
