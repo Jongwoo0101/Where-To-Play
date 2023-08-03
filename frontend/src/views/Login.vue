5cc8f3979e28471f3679b2e9a257e425cfaaea63
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-08-03 23:09:42 +0900

    인풋 디자인 변경 초안

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,12 +1,47 @@
 <template>
     <div class="form">
-        <input type="text" v-model="id" placeholder="ID 또는 Email" required>
+        <input type="text" class="input" required>
+        <label class="user-id">ID 또는 Email</label>
         <input type="password" v-model="pw" placeholder="PW" required>
         <button @click="submit">Login</button>
     </div>
 </template>
 
 <style>
+.form {
+    position: relative;
+}
+
+.input {
+    border: solid 1.5px #000000;
+    border-radius: 1rem;
+    background: none;
+    padding: 1rem;
+    font-size: 1rem;
+    color: #000000;
+    transition: border 150ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.user-id {
+    position: absolute;
+    left: 15px;
+    color: #000000;
+    pointer-events: none;
+    transform: translateY(1rem);
+    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.input:focus, input:valid {
+    outline: none;
+    border: 1.5px solid #1a73e8;
+}
+
+.input:focus ~ label, input:valid ~ label {
+    transform: translateY(-50%) scale(0.8);
+    background-color: #FFFFFF;
+    padding: 0.2em;
+    color: #2196f3;
+}
 
 </style>
 
