794fe2760edf3b1c965ecceb7ad1f83612255fdf
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-08-15 00:34:49 +0900

    메인 페이지 문구 수정 & 100 commit 축하

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <div class="content is-medium">
-      <h2>반갑습니다, WhereToPlay입니다.</h2>
-      <p>당신에게 알맞는 운동장을 실시간으로 확인해보세요!</p>
+      <h2>Where To Play, Where You Want</h2>
+      <p>여러분에게 알맞는 운동장을 실시간으로 확인해보세요!</p><br>
       <RouterLink to="/findplace"><button class="button is-link">지금 시작하기</button></RouterLink>
     </div>
     <p style="position: absolute; color: #969696; right: 2em; bottom: 1em; text-align: right;">Developed by 자료구조, Copyright(c) All rights reserved 2023.</p>
