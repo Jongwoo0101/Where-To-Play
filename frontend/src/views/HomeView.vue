fc404f4d578f19501970e41cb8714ce209b1e68e
Spooder02 <yoshi7591@naver.com>
2023-08-15 18:41:24 +0900

    메인 페이지 UI 수정

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="content is-medium">
       <h2>Where To Play, Where You Want</h2>
-      <p>여러분에게 알맞는 운동장을 실시간으로 확인해보세요!</p><br>
+      <p style="margin-bottom: 0.7em;">여러분에게 알맞는 운동장을 실시간으로 확인해보세요!</p>
       <RouterLink to="/findplace"><button class="button is-link">지금 시작하기</button></RouterLink>
     </div>
     <p style="position: absolute; color: #969696; right: 2em; bottom: 1em; text-align: right;">Developed by 자료구조, Copyright(c) All rights reserved 2023.</p>
