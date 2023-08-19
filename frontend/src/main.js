22d132f6d8988e1045fe6c9339f6ba25d205c12e
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-08-20 01:08:22 +0900

    ✅ vue 기본 pwa 패키지로 변경 & 아이콘 수정 필요! & 로컬 테스트  완료

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import PrimeVue from 'primevue/config'
+import './registerServiceWorker'
 
 createApp(App).use(store).use(router).use(PrimeVue).mount('#app')
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,5 +7,107 @@ module.exports = {
         devMiddleware: {
             publicPath: ''
         },
-    }
+    },
+    pwa: {
+        manifestOptions: {
+            name: "WhereToPlay",
+            short_name: "WhereToPlay",
+            start_url: "./",
+            display: "fullscreen",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            icons: [
+                {
+                    "src": "img/icons/favicon-16x16.png",
+                    "type": "image/png",
+                    "sizes": "16x16",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/favicon-32x32.png",
+                    "type": "image/png",
+                    "sizes": "32x32",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/msapplication-icon-144x144.png",
+                    "type": "image/png",
+                    "sizes": "144x144",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/mstile-150x150.png",
+                    "type": "image/png",
+                    "sizes": "150x150",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/android-chrome-192x192.png",
+                    "type": "image/png",
+                    "sizes": "192x192",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/android-chrome-512x512.png",
+                    "type": "image/png",
+                    "sizes": "512x512",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/android-chrome-maskable-192x192.png",
+                    "type": "image/png",
+                    "sizes": "192x192",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/android-chrome-maskable-512x512.png",
+                    "type": "image/png",
+                    "sizes": "512x512",
+                    "purpose": "any"
+                },
+                {
+                  "src": "img/icons/apple-touch-icon-60x60.png",
+                  "type": "image/png",
+                  "sizes": "60x60",
+                  "purpose": "any"
+                },
+                {
+                  "src": "img/icons/apple-touch-icon-76x76.png",
+                  "type": "image/png",
+                  "sizes": "76x76",
+                  "purpose": "any"
+                },
+                {
+                  "src": "img/icons/apple-touch-icon-120x120.png",
+                  "type": "image/png",
+                  "sizes": "120x120",
+                  "purpose": "any"
+                },
+                {
+                  "src": "img/icons/apple-touch-icon-152x152.png",
+                  "type": "image/png",
+                  "sizes": "152x152",
+                  "purpose": "any"
+                },
+                {
+                  "src": "img/icons/apple-touch-icon-180x180.png",
+                  "type": "image/png",
+                  "sizes": "180x180",
+                  "purpose": "any"
+                },
+            ],
+        },
+    
+        themeColor: "#ffffff",
+        msTileColor: "#ffffff",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        },
+      },
 }
