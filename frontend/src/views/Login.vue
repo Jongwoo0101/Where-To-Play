037455225695179b1638dd73323cdb1942d892b0
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-08-21 10:56:46 +0900

    로그인 창 UI 수정 모바일 대응 ⭕️

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,138 +1,136 @@
 <template>
-    <div class="container">
-        <div class="form">
-        <input type="text" class="id" v-model="id" autofocus>
-        <label class="user-id">ID 또는 Email</label>
-        <input type="password" class="pw" v-model="pw">
-        <label class="user-pw">비밀번호</label>
-        <button @click="submit">Login</button>
+    <div class="login-container">
+      <div class="login-form">
+        <h2>로그인</h2>
+        <div class="input-group">
+          <input type="text" class="input-field" v-model="id" autofocus required>
+          <label class="input-label">ID or Email</label>
         </div>
+        <div class="input-group">
+          <input type="password" class="input-field" v-model="pw" required>
+          <label class="input-label">Password</label>
+        </div>
+        <button @click="submit">Login</button>
+      </div>
     </div>
 </template>
-
+  
 <style scoped>
-.container {
+.login-container {
     display: flex;
     justify-content: center;
-    
+    align-items: center;
+    height: 100vh;
+    background-color: #f2f2f2;
+    background-image: url('@/assets/background.jpeg');
 }
-
-.form {
-    width: 50%;
-    top: 5rem;
-    display: grid;
-    position: relative;
-    background: #FFFFFF;
-}
-
-.id {
-    border: solid 1.5px #000000;
-    border-radius: 1rem;
-    background: none;
-    padding: 1rem;
-    font-size: 1rem;
-    color: #000000;
-    transition: border 150ms cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-.user-id {
-    position: absolute;
-    left: 15px;
-    color: #000000;
-    pointer-events: none;
-    transform: translateY(1rem);
-    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-.id:focus, .id:valid {
-    outline: none;
-    border: 1.5px solid #1a73e8;
-}
-
-.id:focus ~ .user-id, .id:valid ~ .user-id {
-    transform: translateY(-50%) scale(0.8);
-    background-color: #FFFFFF;
-    padding: 0.2em;
-    color: #2196f3;
-}
-
-.pw {
-    border: solid 1.5px #000000;
-    border-radius: 1rem;
-    background: none;
-    padding: 1rem;
-    font-size: 1rem;
-    color: #000000;
-    transition: border 150ms cubic-bezier(0.4, 0, 0.2, 1);
-    margin-top: 2rem;
-}
-
-.user-pw {
-    position: absolute;
-    left: 15px;
-    top: 5.5rem;
-    color: #000000;
-    pointer-events: none;
-    transform: translateY(1rem);
-    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-.pw:focus, .pw:valid {
-    outline: none;
-    border: 1.5px solid #1a73e8;
-}
-
-.pw:focus ~ .user-pw, .pw:valid ~ .user-pw {
-    transform: translateY(-50%) scale(0.8);
-    background-color: #FFFFFF;
-    padding: 0.2em;
-    color: #2196f3;
-}
-
-button {
-    border: 0;
+  
+.login-form {
+    width: 400px;
+    padding: 2rem;
+    background-color: #ffffff;
+    border-radius: 10px;
     box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-    padding: 1rem;
-    border-radius: 1rem;
-    transition: background-color 0.5s;
-    background-color: azure;
-    margin: 1rem 0 0 0;
 }
-
-button:hover {
-    transition: background-color 0.5s;
+  
+.login-form h2 {
+    font-size: 24px;
+    margin-bottom: 1rem;
+    text-align: center;
+}
+  
+.input-group {
+    position: relative;
+    margin-bottom: 1.5rem;
+}
+  
+.input-field {
+    width: 100%;
+    padding: 10px;
+    border: none;
+    border-bottom: 1px solid #ccc;
+    font-size: 16px;
+    background-color: transparent;
+    transition: border-color 0.3s ease;
+}
+  
+.input-label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    font-size: 16px;
+    color: #999;
+    pointer-events: none;
+    transition: transform 0.3s ease, font-size 0.3s ease, color 0.3s ease;
+}
+  
+.input-field:focus,
+.input-field:valid {
+    outline: none;
+    border-color: #1a73e8;
+}
+  
+.input-field:focus ~ .input-label,
+.input-field:valid ~ .input-label {
+    transform: translateY(-120%) scale(0.75);
+    font-size: 12px;
+    color: #1a73e8;
+}
+  
+button {
+    width: 100%;
+    padding: 12px;
+    border: none;
+    border-radius: 8px;
+    font-size: 16px;
+    color: #ffffff;
     background-color: #1a73e8;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
 }
-</style>
+  
+button:hover {
+    background-color: #0e59b3;
+}
 
+</style>
+  
 <script>
-    import axios from 'axios'
-    export default {
-        name: "Login",
-        data() {
-            return {
-                id: null,
-                pw: null,
-            }
-        },
-        methods: {
-            submit() {
-                axios.post(process.env.VUE_APP_BACKEND_ADDRESS+"/auth/login/",{
-                    "username": this.id,
-                    "password": this.pw
-                }, {
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                })
-                .then( response => {
-                    sessionStorage.setItem('nickname', response.data.nickname.nickname)
-                    sessionStorage.setItem('userToken', response.data.token)
-                    sessionStorage.setItem('level', response.data.level)
-                    this.$router.push("/")
-                })
-                .catch( response => { alert("로그인 정보를 다시 확인하세요")})
+  import axios from 'axios';
+  export default {
+    name: "Login",
+    data() {
+      return {
+        id: "",
+        pw: ""
+      };
+    },
+    methods: {
+      submit() {
+        axios
+          .post(
+            process.env.VUE_APP_BACKEND_ADDRESS + "/auth/login/",
+            {
+              username: this.id,
+              password: this.pw
             },
-        }
+            {
+              headers: {
+                "Content-Type": "application/json"
+              }
+            }
+          )
+          .then(response => {
+            sessionStorage.setItem("nickname", response.data.nickname.nickname);
+            sessionStorage.setItem("userToken", response.data.token);
+            sessionStorage.setItem("level", response.data.level);
+            this.$router.push("/");
+          })
+          .catch(response => {
+            alert("로그인 정보를 다시 확인하세요");
+          });
+      }
     }
+  };
 </script>
+  
