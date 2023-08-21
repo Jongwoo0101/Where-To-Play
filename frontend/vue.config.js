ddba67a6ce3bce38dad0ca57db73168753c0b22e
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-08-22 00:06:59 +0900

    ➡️ 아이콘 경로 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -18,79 +18,79 @@ module.exports = {
             background_color: "#ffffff",
             icons: [
                 {
-                    "src": "img/icons/favicon-16x16.png",
+                    "src": "/img/icons/favicon-16x16.png",
                     "type": "image/png",
                     "sizes": "16x16",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/favicon-32x32.png",
+                    "src": "/img/icons/favicon-32x32.png",
                     "type": "image/png",
                     "sizes": "32x32",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/msapplication-icon-144x144.png",
+                    "src": "/img/icons/msapplication-icon-144x144.png",
                     "type": "image/png",
                     "sizes": "144x144",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/mstile-150x150.png",
+                    "src": "/img/icons/mstile-150x150.png",
                     "type": "image/png",
                     "sizes": "150x150",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/android-chrome-192x192.png",
+                    "src": "/img/icons/android-chrome-192x192.png",
                     "type": "image/png",
                     "sizes": "192x192",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/android-chrome-512x512.png",
+                    "src": "/img/icons/android-chrome-512x512.png",
                     "type": "image/png",
                     "sizes": "512x512",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/android-chrome-maskable-192x192.png",
+                    "src": "/img/icons/android-chrome-maskable-192x192.png",
                     "type": "image/png",
                     "sizes": "192x192",
                     "purpose": "any"
                 },
                 {
-                    "src": "img/icons/android-chrome-maskable-512x512.png",
+                    "src": "/img/icons/android-chrome-maskable-512x512.png",
                     "type": "image/png",
                     "sizes": "512x512",
                     "purpose": "any"
                 },
                 {
-                  "src": "img/icons/apple-touch-icon-60x60.png",
+                  "src": "/img/icons/apple-touch-icon-60x60.png",
                   "type": "image/png",
                   "sizes": "60x60",
                   "purpose": "any"
                 },
                 {
-                  "src": "img/icons/apple-touch-icon-76x76.png",
+                  "src": "/img/icons/apple-touch-icon-76x76.png",
                   "type": "image/png",
                   "sizes": "76x76",
                   "purpose": "any"
                 },
                 {
-                  "src": "img/icons/apple-touch-icon-120x120.png",
+                  "src": "/img/icons/apple-touch-icon-120x120.png",
                   "type": "image/png",
                   "sizes": "120x120",
                   "purpose": "any"
                 },
                 {
-                  "src": "img/icons/apple-touch-icon-152x152.png",
+                  "src": "/img/icons/apple-touch-icon-152x152.png",
                   "type": "image/png",
                   "sizes": "152x152",
                   "purpose": "any"
                 },
                 {
-                  "src": "img/icons/apple-touch-icon-180x180.png",
+                  "src": "/img/icons/apple-touch-icon-180x180.png",
                   "type": "image/png",
                   "sizes": "180x180",
                   "purpose": "any"
