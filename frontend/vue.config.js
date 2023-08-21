cc8ed762df2708afd576e28298656994973d3845
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-08-21 13:55:47 +0900

    아이콘 수정 완료 ✅

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,68 +9,105 @@ module.exports = {
         },
     },
     pwa: {
-        manifestOptions: {
-            name: "WhereToPlay",
-            short_name: "WhereToPlay",
-            start_url: "./",
-            display: "fullscreen",
-            theme_color: "#ffffff",
-            background_color: "#ffffff",
-            icons: [
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-72x72.png",
-                  "sizes": "72x72",
+      manifestOptions: {
+          name: "WhereToPlay",
+          short_name: "WhereToPlay",
+          start_url: "./",
+          display: "fullscreen",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          icons: [
+              {
+                  "src": "img/icons/favicon-16x16.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-96x96.png",
-                  "sizes": "96x96",
+                  "sizes": "16x16",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/favicon-32x32.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-128x128.png",
-                  "sizes": "128x128",
+                  "sizes": "32x32",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/msapplication-icon-144x144.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-144x144.png",
                   "sizes": "144x144",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/mstile-150x150.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-152x152.png",
-                  "sizes": "152x152",
+                  "sizes": "150x150",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/android-chrome-192x192.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-192x192.png",
                   "sizes": "192x192",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/android-chrome-512x512.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-384x384.png",
-                  "sizes": "384x384",
-                  "type": "image/png",
-                  "purpose": "maskable any"
-                },
-                {
-                  "src": "frontend/public/app-icons/assets/icons/icon-512x512.png",
                   "sizes": "512x512",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/android-chrome-maskable-192x192.png",
                   "type": "image/png",
-                  "purpose": "maskable any"
-                }
-              ]
-        },
-    
-        themeColor: "#ffffff",
-        msTileColor: "#ffffff",
-        appleMobileWebAppCapable: "yes",
-        appleMobileWebAppStatusBarStyle: "black",
+                  "sizes": "192x192",
+                  "purpose": "any"
+              },
+              {
+                  "src": "img/icons/android-chrome-maskable-512x512.png",
+                  "type": "image/png",
+                  "sizes": "512x512",
+                  "purpose": "any"
+              },
+              {
+                "src": "img/icons/apple-touch-icon-60x60.png",
+                "type": "image/png",
+                "sizes": "60x60",
+                "purpose": "any"
+              },
+              {
+                "src": "img/icons/apple-touch-icon-76x76.png",
+                "type": "image/png",
+                "sizes": "76x76",
+                "purpose": "any"
+              },
+              {
+                "src": "img/icons/apple-touch-icon-120x120.png",
+                "type": "image/png",
+                "sizes": "120x120",
+                "purpose": "any"
+              },
+              {
+                "src": "img/icons/apple-touch-icon-152x152.png",
+                "type": "image/png",
+                "sizes": "152x152",
+                "purpose": "any"
+              },
+              {
+                "src": "img/icons/apple-touch-icon-180x180.png",
+                "type": "image/png",
+                "sizes": "180x180",
+                "purpose": "any"
+              },
+          ],
       },
+
+      themeColor: "#ffffff",
+      msTileColor: "#ffffff",
+      appleMobileWebAppCapable: "yes",
+      appleMobileWebAppStatusBarStyle: "black",
+      iconPaths: {
+          favicon32: 'img/icons/favicon-32x32.png',
+          favicon16: 'img/icons/favicon-16x16.png',
+          appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+          maskIcon: 'img/icons/safari-pinned-tab.svg',
+          msTileImage: 'img/icons/msapplication-icon-144x144.png'
+      },
+    },
 }
